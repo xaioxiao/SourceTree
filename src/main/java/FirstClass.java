ec7ed9da4e25701ab3d640339e6bbe8b2732f804
聆听，与你产生共鸣 <122902156@qq.com>
2019-12-13 23:30:21 +0800

    新增加的内容

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,2 +1,7 @@
 public class FirstClass {
+    public static void main(String[] args) {
+        boolean isSuccess = true;
+        throw new IllegalArgumentException("illegal argument");
+    }
 }
+
