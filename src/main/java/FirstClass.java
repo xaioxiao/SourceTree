ffe76d0f6672a63f66190801d0a68a50411f87da
聆听，与你产生共鸣 <122902156@qq.com>
2019-12-14 10:59:31 +0800

    这是一个好的东西，需要学习

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -2,6 +2,9 @@ public class FirstClass {
     public static void main(String[] args) {
         boolean isSuccess = true;
         throw new IllegalArgumentException("illegal argument");
+        for (int i = 0; i <4; i++) {
+            i+=1;
+        }
     }
 }
 
